649a1e9cf9ca62e67bba065b611a1b91e01bfea0
nvacan3 <nicolas.b.moise@gmail.com>
2023-06-12 16:51:19 -0400

    creates board

---
+++ client/src/pages/board.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Container } from "@mui/material";
+import "./board.css";
+
+const Board = () => {
+  const dimension = {
+    height: 10,
+    width: 10,
+  };
+
+  const array2D = Array(dimension.width)
+    .fill()
+    .map((_, indexH) =>
+      Array(dimension.height)
+        .fill()
+        .map((_, indexW) => indexW)
+    );
+  console.log("array2D: ", array2D);
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className="center">
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${dimension.width}, 30px)`,
+            gridTemplateRows: `repeat(${dimension.height}, 30px)`,
+          }}
+        >
+          {array2D.map((row, i) =>
+            row.map((col, j) => (
+              <div className="Grids" key={`${i}-${j}`}>
+                {col}
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Board;
